4554a1f9bad18dab0a87619b9fd8a8a948cbcee9
Ferdie Nodalo <ferdienodalo7@gmail.com>
2023-08-14 17:32:12 +0800

    Adding short-link project from frontend mentor challenge

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,6 +10,43 @@ const Project = () => {
 					<div className='project_headings'>
 						<p>Featured Project</p>
 						<h2>
+							<a href='https://short-link-lyart.vercel.app/'>
+								URL shortening API landing page
+							</a>
+						</h2>
+					</div>
+					<div className='project_contents'>
+						<p>
+							Seamlessly integrate with the
+							<a href='https://shrtco.de/tools/shorten'> shrtcode</a> URL
+							shortening API for streamlined link transformation. Converting
+							long URLs into concise, shareable links.
+						</p>
+						<ul className='technologies_used'>
+							<li>
+								<i className='bx bxl-react'></i> <span>ReactJS</span>
+							</li>
+							<li>
+								<span>API</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className='project_image-container'>
+					<a href='https://short-link-lyart.vercel.app/'>
+						<img
+							src={require('../assets/image/url_shortening_api.png')}
+							alt=''
+						/>
+					</a>
+				</div>
+			</div>
+
+			<div className='projects_container'>
+				<div className='project_text-container'>
+					<div className='project_headings'>
+						<p>Featured Project</p>
+						<h2>
 							<a href='https://classroom-app-fe.vercel.app/'>
 								Classroom Management App
 							</a>
